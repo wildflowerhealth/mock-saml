c6a99d63a5374d5e757960fe7f16c57d7bae98eb
Noah Glusenkamp <noah.glusenkamp@wildflowerhealth.com>
2025-03-06 13:27:18 -0800

    pretty

--- pages/saml/sydney.tsx
+++ pages/saml/sydney.tsx
@@ -380,7 +380,9 @@ export default function Sydney() {
                     </div>
                   </div>
 
-                  <button className='btn btn-primary block mt-4' disabled={Boolean(jsonError)}>Launch Wildflower</button>
+                  <button className='btn btn-primary block mt-4' disabled={Boolean(jsonError)}>
+                    Launch Wildflower
+                  </button>
                 </div>
               </form>
             </div>
@@ -398,9 +400,9 @@ export default function Sydney() {
                   Copy
                 </button>
               </div>
-              { jsonError && (
+              {jsonError && (
                 <div className='bg-red-100 border border-red-400 text-red-700 px-4 py-2 rounded mb-2'>
-                    {jsonError}
+                  {jsonError}
                 </div>
               )}
               <textarea
