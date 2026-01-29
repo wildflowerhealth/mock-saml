d1fdca1bdaa18671393b26af223f941c24d97c29
Andrew Mitchell <andrewgeorgemitchell@gmail.com>
2026-01-29 08:42:00 -0800

    Update UAT URLs in settings

--- utils/settings.ts
+++ utils/settings.ts
@@ -41,8 +41,8 @@ const samlConfigMap: Record<WfhEnv, SamlConfig> = {
     agApiKey: process.env.AG_API_KEY ?? 'e020b2a6-30af-46f6-9524-c33dc0598461',
   },
   uat: {
-    acs: 'https://anthem-rnapp.uat.wildflowerhealth.digital' + acsPath,
-    acsCordovaWebAppDomain: 'https://anthem-staging.buildinghealthyfamilies.ai' + acsPath,
+    acs: 'https://anthem-staging.buildinghealthyfamilies.ai,' + acsPath,
+    acsCordovaWebAppDomain: 'https://anthem-cordova.uat.wildflowerhealth.digital' + acsPath,
     audience: 'com.wildflowerhealth.saml.uat',
   },
   prod: {
